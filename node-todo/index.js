c09092cfb4cf606ae97dae27f77de336d35a11e8
MccSx <shixiangcool@163.com>
2020-07-27 15:59:56 +0800

    优化

--- node-todo/index.js
+++ node-todo/index.js
@@ -1,68 +1,7 @@
 const inquirer = require("inquirer");
 const db = require("./db.js");
 
-function choicesAction(list, index) {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "请选择你要的操作",
-        choices: [
-          { name: "退出", value: "quit" },
-          { name: "已完成", value: "isFinished" },
-          { name: "未完成", value: "unFinisded" },
-          { name: "改标题", value: "updateTitle" },
-          { name: "删除", value: "deleteTask" }
-        ]
-      }
-    ])
-    .then(answers => {
-      switch (answers.action) {
-        case "isFinished":
-          list[index].done = true;
-          db.write(list);
-          break;
-        case "unFinished":
-          list[index].done = false;
-          db.write(list);
-          break;
-        case "updateTitle":
-          inquirer
-            .prompt([
-              {
-                type: "input",
-                name: "new_task_name",
-                message: "输入新的任务名称"
-              }
-            ])
-            .then(async answers => {
-              list[index].title = answers.new_task_name;
-              db.write(list);
-            });
-          break;
-        case "deleteTask":
-          list.splice(index, 1);
-          db.write(list);
-          break;
-        default:
-          break;
-      }
-    });
-}
-
-module.exports.add = async name => {
-  const list = await db.read();
-  list.push({ title: name, done: false });
-  await db.write(list);
-};
-
-module.exports.clear = async () => {
-  await db.write([]);
-};
-
-module.exports.showAll = async () => {
-  const list = await db.read();
+function getAllTasks(list) {
   inquirer
     .prompt([
       {
@@ -88,18 +27,90 @@ module.exports.showAll = async () => {
         choicesAction(list, index);
       } else if (index === -2) {
         // 选择创建任务
-        inquirer
-          .prompt([
-            {
-              type: "input",
-              name: "task_name",
-              message: "输入你的任务名称"
-            }
-          ])
-          .then(answers => {
-            list.push({ title: answers.task_name, done: false });
-            db.write(list);
-          });
+        addNewTask(list);
       }
     });
+}
+
+function choicesAction(list, index) {
+  const actions = { isFinished, unFinished, updateTitle, deleteTask };
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "请选择你要的操作",
+        choices: [
+          { name: "退出", value: "quit" },
+          { name: "已完成", value: "isFinished" },
+          { name: "未完成", value: "unFinished" },
+          { name: "改标题", value: "updateTitle" },
+          { name: "删除", value: "deleteTask" }
+        ]
+      }
+    ])
+    .then(answers => {
+      const action = actions[answers.action];
+      action && action(list, index);
+    });
+}
+
+function addNewTask(list) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "task_name",
+        message: "输入你的任务名称"
+      }
+    ])
+    .then(answers => {
+      list.push({ title: answers.task_name, done: false });
+      db.write(list);
+    });
+}
+
+function isFinished(list, index) {
+  list[index].done = true;
+  db.write(list);
+}
+
+function unFinished(list, index) {
+  list[index].done = false;
+  db.write(list);
+}
+
+function updateTitle(list, index) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "new_task_name",
+        message: "输入新的任务名称"
+      }
+    ])
+    .then(async answers => {
+      list[index].title = answers.new_task_name;
+      db.write(list);
+    });
+}
+
+function deleteTask(list, index) {
+  list.splice(index, 1);
+  db.write(list);
+}
+
+module.exports.add = async name => {
+  const list = await db.read();
+  list.push({ title: name, done: false });
+  await db.write(list);
+};
+
+module.exports.clear = async () => {
+  await db.write([]);
+};
+
+module.exports.showAll = async () => {
+  const list = await db.read();
+  getAllTasks(list);
 };

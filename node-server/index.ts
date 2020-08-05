903afbf4af9148d1ac49415d333b31b42251c7ab
MccSx <shixiangcool@163.com>
2020-08-05 15:49:31 +0800

    done

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,13 +1,16 @@
 import * as http from "http";
 import * as p from "path";
 import * as fs from "fs";
+import * as url from "url";
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, "public");
 
 server.on("request", (request, response) => {
-  const { url, method, headers } = request;
-  switch (url) {
+  const { url: path, method, headers } = request;
+  const { pathname } = url.parse(path);
+  console.log(url.parse(path));
+  switch (pathname) {
     case "/index.html":
       fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
         if (error) {

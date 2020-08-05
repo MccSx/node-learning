3c806914d7a583986521d73003864f6ceff5087b
MccSx <shixiangcool@163.com>
2020-08-05 11:57:00 +0800

    read html

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,10 +1,41 @@
 import * as http from "http";
+import * as p from "path";
+import * as fs from "fs";
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, "public");
 
 server.on("request", (request, response) => {
-  console.log("有请求了");
-  response.end("hello");
+  const { url, method, headers } = request;
+  switch (url) {
+    case "/index.html":
+      fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
+        if (error) {
+          throw error;
+        }
+        response.end(data.toString());
+      });
+      break;
+    case "/style.css":
+      fs.readFile(p.resolve(publicDir, "style.css"), (error, data) => {
+        if (error) {
+          throw error;
+        }
+        response.end(data.toString());
+      });
+      break;
+    case "/main.js":
+      fs.readFile(p.resolve(publicDir, "main.js"), (error, data) => {
+        if (error) {
+          throw error;
+        }
+        response.end(data.toString());
+      });
+      break;
+    default:
+      response.end();
+      break;
+  }
 });
 
 server.listen(8888);
